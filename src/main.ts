f9f8665e0abff739d0a498544f309a7ba78d9714
yyhhenry <yyhhenry@foxmail.com>
2023-02-27 09:52:46 +0800

    highlight

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import hljs from 'highlight.js/lib/core';
+import cpp from 'highlight.js/lib/languages/cpp';
+import plaintext from 'highlight.js/lib/languages/plaintext';
 import 'highlight.js/styles/github-dark.css';
-import 'highlight.js/lib/common';
 import highlight from '@highlightjs/vue-plugin';
 
 import 'element-plus/theme-chalk/src/index.scss';
 import './assets/main.css';
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('txt', plaintext);
 
 const app = createApp(App);
 app.use(highlight);
