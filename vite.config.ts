dd91deb346ea7cb85bddc85bb3f06dee2675a38e
yyhhenry <yyhhenry@foxmail.com>
2023-03-03 11:26:25 +0800

    重定向

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/data-structure-homework-ui-1',
+    base: '/data-structure-homework-ui-1/dist',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
