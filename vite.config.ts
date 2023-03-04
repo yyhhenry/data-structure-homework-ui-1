aa23a0b7c86ba3b29fb29c2f8f24088eb003d0d4
yyhhenry <yyhhenry@foxmail.com>
2023-03-04 10:59:25 +0800

    搬迁docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/data-structure-homework-ui-1/dist',
+    build: {
+        emptyOutDir: true,
+        outDir: path.join(__dirname, 'docs'),
+    },
+    base: '/data-structure-homework-ui-1',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
